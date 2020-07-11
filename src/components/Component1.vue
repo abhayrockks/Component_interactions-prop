43b35a1f48e4d9efe2b9d144d983cc88afaa6e34
Abhay Mehta <abhayrockks@gmail.com>
2020-07-11 17:54:18 +0530

    second commit for src file

---
+++ src/components/Component1.vue
@@ -0,0 +1,23 @@
+<template>
+  <child-component :myProp="preciousThing"></child-component>
+</template>
+
+<script>
+import Component2 from "./Component2";
+
+export default {
+  name: "Component1",
+  components: {
+    "child-component": Component2
+  },
+  data() {
+    return {
+      preciousThing: "ring"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/Component2.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>The precious thing: {{myProp}}</h2>
+</template>
+
+<script>
+export default {
+  name: "Component2",
+  props: ['myProp']
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
